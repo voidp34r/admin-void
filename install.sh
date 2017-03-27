fb1ea3a2d57f5358bb505313fbf617c366e67ea7
voidmind <matheus@matheusr.com>
2017-03-27 08:32:50 -0300

    add init.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #/bin/bash
 
-echo "deb http://download.rethinkdb.com/apt wheezy main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-echo "deb http://download.rethinkdb.com/apt wheezy main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-echo "deb http://download.rethinkdb.com/apt wheezy main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-echo "deb http://download.rethinkdb.com/apt wheezy main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
-wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
+sudo echo "deb http://download.rethinkdb.com/apt jessie main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+sudo echo "deb http://download.rethinkdb.com/apt jessie main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+sudo echo "deb http://download.rethinkdb.com/apt jessie main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+sudo echo "deb http://download.rethinkdb.com/apt jessie main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list
+sudo wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -
 
-echo "deb http://dl.bintray.com/deepstreamio/deb wheezy main" | sudo tee -a /etc/apt/sources.list
-echo "deb http://dl.bintray.com/deepstreamio/deb wheezy main" | sudo tee -a /etc/apt/sources.list
-echo "deb http://dl.bintray.com/deepstreamio/deb wheezy main" | sudo tee -a /etc/apt/sources.list
-echo "deb http://dl.bintray.com/deepstreamio/deb wheezy main" | sudo tee -a /etc/apt/sources.list
+sudo echo "deb http://dl.bintray.com/deepstreamio/deb jessie main" | sudo tee -a /etc/apt/sources.list
+sudo echo "deb http://dl.bintray.com/deepstreamio/deb jessie main" | sudo tee -a /etc/apt/sources.list
+sudo echo "deb http://dl.bintray.com/deepstreamio/deb jessie main" | sudo tee -a /etc/apt/sources.list
+sudo echo "deb http://dl.bintray.com/deepstreamio/deb jessie main" | sudo tee -a /etc/apt/sources.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 
 sudo apt-get update
 # sudo apt-get -y upgrade
-sudo apt-get install build-essential protobuf-compiler python \
+sudo apt-get install -y build-essential protobuf-compiler python \
                      libprotobuf-dev libcurl4-openssl-dev \
                      libboost-all-dev libncurses5-dev \
                      libjemalloc-dev wget m4
@@ -22,11 +22,11 @@ sudo apt-get install -y chkconfig tmux ntsysv
 sudo apt-get install -y deepstream.io
 sudo apt-get install -y rethinkdb
 
-sudo git clone git://git.code.sf.net/p/tmux/tmux-code tmux
-cd tmux
-sudo sh autogen.sh
-sudo ./configure && make  
-sudo apt-get update
+# sudo git clone git://git.code.sf.net/p/tmux/tmux-code tmux
+# cd tmux
+# sudo sh autogen.sh
+# sudo ./configure && make  
+# sudo apt-get update
 echo "feito!"
 
 
